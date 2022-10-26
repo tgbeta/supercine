7c5731e5727241253c42814c34164c475e4556b9
Tatiana <tatianabohrer@gmail.com>
2022-10-25 20:00:21 -0400

    start search movie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SearchMovie from "./components/SearchMovie.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SearchMovie />
+    </div>
+  );
 }
 
 export default App;
