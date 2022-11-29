de88ec49ee783f998ea53f249638a2e30bcb940c
Clelia Marcia Miguel <99687130+cleliamarcia@users.noreply.github.com>
2022-11-28 16:10:48 -0800

    Update WatchListRoute.js

--- backend/routes/WatchListRoute.js
+++ backend/routes/WatchListRoute.js
@@ -8,7 +8,7 @@ const {
 } = require("../controllers/WatchListController.js");
 
 router.get("/", showWatchList);
-// router.post("/:id", addWatchList);
-// router.post("/:id/delete", deleteWatchList);
+router.post("/:id", addWatchList);
+router.post("/:id/delete", deleteWatchList);
 
 module.exports = router;
